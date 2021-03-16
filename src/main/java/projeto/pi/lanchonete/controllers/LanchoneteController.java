09040a093ce1ba23e93fef01c02b92a422808abe
deborabia <deborabeatrizcamara@gmail.com>
2021-03-16 16:18:08 -0300

    criando controler de lanchonete (correto)

--- src/main/java/projeto/pi/lanchonete/controllers/LanchoneteController.java
+++ src/main/java/projeto/pi/lanchonete/controllers/LanchoneteController.java
@@ -1,5 +1,15 @@
 package projeto.pi.lanchonete.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class LanchoneteController {
+	
+	@RequestMapping("/lanchonete/form")
+	private String form() {
+		return "formLanchonete";
+		
+	}
 
 }
