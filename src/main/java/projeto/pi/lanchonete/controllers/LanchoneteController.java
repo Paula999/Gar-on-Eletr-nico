c358313ccff700f2a3400c5aa207357564712670
deborabia <deborabeatrizcamara@gmail.com>
2021-03-16 16:36:03 -0300

    criando formulário para adicionar produto

--- src/main/java/projeto/pi/lanchonete/controllers/LanchoneteController.java
+++ src/main/java/projeto/pi/lanchonete/controllers/LanchoneteController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LanchoneteController {
 	
-	@RequestMapping("/lanchonete/form")
+	@RequestMapping("/produtos/form")
 	private String form() {
-		return "formLanchonete";
+		return "formProduto";
 		
 	}
 
