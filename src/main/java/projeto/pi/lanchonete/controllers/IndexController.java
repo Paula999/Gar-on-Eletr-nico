fefa6f733859e7fa4b1b4404c40cd53a9658561d
deborabia <deborabeatrizcamara@gmail.com>
2021-03-16 15:00:20 -0300

    adicionando os arquivos iniciais

---
+++ src/main/java/projeto/pi/lanchonete/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package projeto.pi.lanchonete.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String index() {
+		
+		return "home";
+	}
+
+}
