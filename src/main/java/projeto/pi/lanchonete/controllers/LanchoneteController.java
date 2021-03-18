2ac6218029aa85b8e4971eea019fc4938cb5af6e
deborabia <deborabeatrizcamara@gmail.com>
2021-03-18 18:08:06 -0300

    Criando repository para produto per inserido no bd

--- src/main/java/projeto/pi/lanchonete/controllers/LanchoneteController.java
+++ src/main/java/projeto/pi/lanchonete/controllers/LanchoneteController.java
@@ -1,16 +1,21 @@
 package projeto.pi.lanchonete.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import projeto.pi.lanchonete.models.produto;
+import projeto.pi.lanchonete.repositories.ProdutoRepository;
 
 
 
 
 @Controller
 public class LanchoneteController {
+	
+	@Autowired
+	private ProdutoRepository pr;
 	
 	@RequestMapping("/produtos/form")
 	private String form() {
@@ -21,6 +26,7 @@ public class LanchoneteController {
 	private String adicionar(produto produto) {
 		
 		System.out.println(produto);
+		pr.save(produto);
 		return "produtoAdicionado";
 		
 	}
