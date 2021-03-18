f1ea5deddf32efc82005e186c24fb50d26e1cedc
deborabia <deborabeatrizcamara@gmail.com>
2021-03-17 22:22:01 -0300

    configurando classe de modelo produto

--- src/main/java/projeto/pi/lanchonete/models/produto.java
+++ src/main/java/projeto/pi/lanchonete/models/produto.java
@@ -1,9 +1,28 @@
 package projeto.pi.lanchonete.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nome;
 	private double preço;
 	private String descrição;
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
