003402b847432cf665b87a9c48f6b9d99a680df4
deborabia <deborabeatrizcamara@gmail.com>
2021-03-19 15:43:07 -0300

    colocando menu em todas as páginas

--- src/main/java/projeto/pi/lanchonete/repositories/ProdutoRepository.java
+++ src/main/java/projeto/pi/lanchonete/repositories/ProdutoRepository.java
@@ -1,7 +1,6 @@
 package projeto.pi.lanchonete.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import projeto.pi.lanchonete.models.produto;
 
 public interface ProdutoRepository extends JpaRepository<produto, Long> {
