b5c0635d7b86d6462729369a6f1537f3caa516f6
deborabia <deborabeatrizcamara@gmail.com>
2021-03-20 12:22:13 -0300

    listando itens do cardápio

--- src/main/java/projeto/pi/lanchonete/controllers/LanchoneteController.java
+++ src/main/java/projeto/pi/lanchonete/controllers/LanchoneteController.java
@@ -1,9 +1,13 @@
 package projeto.pi.lanchonete.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import projeto.pi.lanchonete.models.produto;
 import projeto.pi.lanchonete.repositories.ProdutoRepository;
@@ -12,22 +16,36 @@ import projeto.pi.lanchonete.repositories.ProdutoRepository;
 
 
 @Controller
+@RequestMapping("/produtos")
 public class LanchoneteController {
 	
 	@Autowired
 	private ProdutoRepository pr;
 	
-	@RequestMapping("/produtos/form")
+	@GetMapping("/form")
 	private String form() {
 		return "formProduto";
 		
 	}
-	@PostMapping("/produtos")
+	@PostMapping
 	private String adicionar(produto produto) {
 		
 		System.out.println(produto);
 		pr.save(produto);
-		return "produtoAdicionado";
+		return "produtos/produtoAdicionado";
+		
+	}
+	
+	@GetMapping
+	public ModelAndView listar () {
+	   List<produto> produtos = pr.findAll();
+	   ModelAndView mv = new ModelAndView("produtos/cardapio");
+	   mv.addObject("produtos", produtos );
+	   return mv;
+	   
+	   
+		
+		
 		
 	}
 
